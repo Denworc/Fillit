51a2830c4e43b9154927a2738f16dba7c7de6510
Admin <admin@Mac-Admin.local>
2017-02-19 16:30:20 +0200

    add error_check

--- main.c
+++ main.c
@@ -1,14 +1,22 @@
 #include "fillit.h"
 
+int	ft_error_check(int n)
+{
+  if (n == 1)
+    ft_putstr("usage: ./fillit.a source_file");
+  else if (n == 2)
+    ft_putstr("error");
+  
+
 int	main(int ac, char **av)
 {
   int	fd; // file descriptor
 
   if (ac != 2)
-    return(); //error with arguments
+    return(ft_error_check(1)); //error with arguments
   fd = open(av[1], O_RDONLY);
   if (fd < 0)
-    return(); //error from file descriptor
+    return(ft_error_check(2)); //error from file descriptor
 
 
 
